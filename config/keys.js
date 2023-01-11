cec7d21f072cdbca577c653bbb0d219bdf176edd
Anas <44112604+anasamin26@users.noreply.github.com>
2023-01-12 01:31:15 +0500

    Delete keys.js

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://ansich:s7edge.com@firstapp-backend.nfvgdje.mongodb.net/?retryWrites=true&w=majority&ssl=true",
-};
-module.exports = {
-  mongoURI:
-    "mongodb+srv://ansich:s7edge.com@firstapp-backend.nfvgdje.mongodb.net/?retryWrites=true&w=majority&ssl=true",
-  secretOrKey: "secret",
-};
